20d2dff0d126ba5ffd667b620c507c28db91879d
b-selvakumar <30803752+b-selvakumar@users.noreply.github.com>
2017-08-17 20:28:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/articleone',function(req, res) {
-  res.send('Article One requested and will be served here..');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));;
 } );
 
 app.get('/articletwo', function(req,res){
