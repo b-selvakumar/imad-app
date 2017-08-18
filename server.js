56c5afbf61eb5134e5b8051427d010617eb9f4b2
b-selvakumar <30803752+b-selvakumar@users.noreply.github.com>
2017-08-18 21:30:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ function createtemplate (data)
 {
  var heading = data.heading;
  var title = data.title;
- var date1 = data.data1;
+ var date1 = data.date1;
  var content = data.content;
  
     var htmltemplate = `
