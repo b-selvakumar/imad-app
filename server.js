596a6d98e09228e2ecf884276f806733dd410d6b
b-selvakumar <30803752+b-selvakumar@users.noreply.github.com>
2017-08-18 22:11:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get(':/articleName',function(req, res) {
+app.get('/:articleName',function(req, res) {
     var articleName = req.params.articleName;
       res.send(createtemplate(articles[articleName]));
 } );
