91fe85a29b37a4c526b8176adaa8ae9551f8d395
b-selvakumar <30803752+b-selvakumar@users.noreply.github.com>
2017-08-25 20:37:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,6 +99,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('article/:articleName',function(req, res) {
+    alert ("test");
     var articleName = req.params.articleName;
     var querystring = "SELECT * FROM ARTICLE WHERE TITLE = '" + req.params.articleName + "'" ;
   
